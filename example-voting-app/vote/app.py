cef33299f9a1f4b3566a67d95de2185a23839c03
Jan Willies <jan.willies@accenture.com>
2016-11-08 18:27:13 +0100

    add new vote

--- example-voting-app/vote/app.py
+++ example-voting-app/vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "systemd")
+option_b = os.getenv('OPTION_B', "SysVinit")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
